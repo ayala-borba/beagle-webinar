724a7e5d5c8e8d0a406469d7623fa04e556c2c43
Hernand Azevedo <hernand.azevedo@zup.com.br>
2020-02-27 10:29:23 -0300

    :tada: Initial Structure without beagle

---
+++ step2/android/app/src/main/java/br/zup/com/beagle/android/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.zup.com.beagle.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
