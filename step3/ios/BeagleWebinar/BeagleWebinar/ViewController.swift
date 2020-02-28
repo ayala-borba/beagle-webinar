d3623f9e618aa6f14be575cec8e1d2a697c0d25e
Fred Franco <frederico.calhau@zup.com.br>
2020-02-28 18:11:17 -0300

    ios step 3: add custom TextField for real

--- step3/ios/BeagleWebinar/BeagleWebinar/ViewController.swift
+++ step3/ios/BeagleWebinar/BeagleWebinar/ViewController.swift
@@ -18,11 +18,33 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
+        let margin = Flex(margin: Flex.EdgeValue(all: UnitValue(value: 10, type: .real)))
+
+        let children: [ServerDrivenComponent] = [
+            FormInput(name: "username", child:
+                TextField(placeHolder: "Usuario", flex: margin)
+            ),
+            FormInput(name: "password", child:
+                TextField(placeHolder: "Senha", flex: margin)
+            ),
+            FormSubmit(child: Button(text: "Entrar", flex: margin))
+        ]
+
+        let content = Container(
+            children: children,
+            flex: Flex(flexDirection: .column, justifyContent: .center, grow: 1),
+            appearance: Appearance(backgroundColor: "#AA1232")
+        )
+
+        let form = Form(path: "submitForm", method: .post, child: content)
+
+
         let beagle = BeagleScreenViewController(viewModel: .init(
             screenType: .declarative(
-                Screen(content: CustomText(text: "Testando"))
+                Screen(content: form)
             )
         ))
+
 
 /// Receiving from server
 //        Beagle.registerCustomComponent(
@@ -35,6 +57,8 @@ class ViewController: UIViewController {
 //            screenType: .remote("http://localhost:8080/custom/testando", fallback: nil)
 //        ))
 /// To be able to use http (without s) we need to allow App Transport Security
+
+        Beagle.registerCustomComponent("TextField", componentType: TextField.self, entityType: TextFieldEntity.self)
 
         beagle.modalPresentationStyle = .fullScreen
         present(beagle, animated: true, completion: nil)
