fcde7c7b4b3ce2e9e70121280639902c3812d97a
Luiz Gustavo Gomes Borges <luiz.borges@zup.com.br>
2020-03-02 17:25:58 -0300

    #backend: improved BFF organization

--- step3/backend/src/main/kotlin/br/com/zup/beagle/demo/service/BeagleService.kt
+++ step3/backend/src/main/kotlin/br/com/zup/beagle/demo/service/BeagleService.kt
@@ -1,6 +1,14 @@
 package br.com.zup.beagle.demo.service
 
+import br.com.zup.beagle.action.*
+import br.com.zup.beagle.demo.dto.request.LoginForm
+import br.com.zup.beagle.demo.ui.ComposeFormLogin
 import br.com.zup.beagle.demo.widget.CustomText
+import br.com.zup.beagle.widget.core.Alignment
+import br.com.zup.beagle.widget.core.Flex
+import br.com.zup.beagle.widget.core.JustifyContent
+import br.com.zup.beagle.widget.layout.NavigationBar
+import br.com.zup.beagle.widget.layout.Screen
 import br.com.zup.beagle.widget.ui.Text
 import org.springframework.stereotype.Service
 
@@ -9,4 +17,38 @@ class BeagleService {
     fun getText(name: String) = Text("Hello, $name!")
 
     fun getCustomText(name: String) = CustomText(name)
+
+    fun createFormLogin() = Screen(
+        navigationBar = NavigationBar(
+            title = "entrar"
+        ),
+        content = ComposeFormLogin()
+    )
+
+    fun validateLogin(loginForm: LoginForm) = when {
+        loginForm.login.isBlank() -> FormValidation(
+            errors = listOf(
+                FieldError("login", "Digite seu login!")
+            )
+        )
+
+        loginForm.password.isBlank() -> FormValidation(
+            errors = listOf(
+                FieldError("password", "Digite sua senha!")
+            )
+        )
+
+        else -> Navigate(type = NavigationType.SWAP_VIEW, path = "/welcome/${loginForm.login}")
+    }
+
+    fun createWelcome(user: String) = Text("Bem-vindo $user!")
+        .applyFlex(
+            flex = Flex(
+                grow = 1.0,
+                justifyContent = JustifyContent.CENTER,
+                alignContent = Alignment.CENTER,
+                alignItems = Alignment.CENTER,
+                alignSelf = Alignment.CENTER
+            )
+        )
 }
