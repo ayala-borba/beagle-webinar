6dc9ce3cdb2109c3b0505b3502871a4e5200ea96
Hernand Azevedo <hernand.azevedo@zup.com.br>
2020-03-02 14:13:51 -0300

    change jackson mapping

--- step2/backend/src/main/kotlin/br/com/zup/beagle/demo/configuration/JacksonConfiguration.kt
+++ step2/backend/src/main/kotlin/br/com/zup/beagle/demo/configuration/JacksonConfiguration.kt
@@ -2,23 +2,25 @@ package br.com.zup.beagle.demo.configuration
 
 import br.com.zup.beagle.serialization.jackson.BeagleActionSerializer
 import br.com.zup.beagle.serialization.jackson.BeagleComponentSerializer
+import br.com.zup.beagle.serialization.jackson.BeagleScreenBuilderSerializer
 import br.com.zup.beagle.serialization.jackson.BeagleScreenSerializer
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 
 @Configuration
 class JacksonConfiguration {
     @Bean
-    fun objectMapper(): ObjectMapper {
-        val mapper = jacksonObjectMapper()
-        val module = SimpleModule()
-        module.addSerializer(BeagleComponentSerializer())
-        module.addSerializer(BeagleActionSerializer())
-        module.addSerializer(BeagleScreenSerializer())
-        mapper.registerModule(module)
-        return mapper
+    open fun jacksonBuilder(): Jackson2ObjectMapperBuilder {
+        return Jackson2ObjectMapperBuilder()
+            .serializationInclusion(JsonInclude.Include.NON_NULL)
+            .serializers(BeagleComponentSerializer())
+            .serializers(BeagleActionSerializer())
+            .serializers(BeagleScreenSerializer())
+            .serializers(BeagleScreenBuilderSerializer())
     }
 }
